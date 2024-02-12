007b600c69ae94b5b4c7902241409f41b8761d16
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-02-11 22:35:53 -0500

    Add the corresponding field to the form

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,8 @@
 const library = [];
 
-let title_input = prompt("Enter your book's title:");
-let author_input = prompt("Enter the author name:");
-let pages_input = prompt("Enter the pages number:");
+// let title_input = prompt("Enter your book's title:");
+// let author_input = prompt("Enter the author name:");
+// let pages_input = prompt("Enter the pages number:");
 
 function Book(title, author, number_of_pages) {
   this.title = title;
